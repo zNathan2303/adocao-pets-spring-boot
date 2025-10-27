f50b66451df964755eee472efba534787ec3b4bb
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-10-27 19:06:25 -0300

    first commit

---
+++ src/main/java/br/dev/nathan/adocao_pets/AdocaoPetsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.nathan.adocao_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/nathan/adocao_pets/AdocaoPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.nathan.adocao_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
