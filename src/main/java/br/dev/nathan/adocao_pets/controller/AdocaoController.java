b45e30d340412f64b4c607e3cca1987edc9fd8de
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-11-06 18:54:20 -0300

    docs(AdocaoController): anotações para configuração do Swagger

--- src/main/java/br/dev/nathan/adocao_pets/controller/AdocaoController.java
+++ src/main/java/br/dev/nathan/adocao_pets/controller/AdocaoController.java
@@ -2,6 +2,11 @@ package br.dev.nathan.adocao_pets.controller;
 
 import br.dev.nathan.adocao_pets.dtos.AdocaoDTO;
 import br.dev.nathan.adocao_pets.services.AdocaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/adocao")
 @Validated
+@Tag(name = "Adoção", description = "Gerenciar as adoções de pets")
 public class AdocaoController {
 
     private final AdocaoService service;
@@ -21,22 +27,62 @@ public class AdocaoController {
     }
 
     @GetMapping
+    @Operation(
+        summary = "Listar adoções",
+        description = "Lista todas as adoções que já ocorreram."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Sucesso!")
+    })
     public ResponseEntity<List<AdocaoDTO>> listarTodasAdocoes() {
         return ResponseEntity.ok(service.listarTudo());
     }
 
     @GetMapping("/{id}")
+    @Operation(
+        summary = "Buscar uma adoção",
+        description = "Busca uma adoção por id."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Sucesso!"),
+        @ApiResponse(responseCode = "404", description = "Adoção não encontrada!", content = @Content())
+    })
     public ResponseEntity<AdocaoDTO> buscarAdocaoPorId(@PathVariable Integer id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
     @PostMapping
+    @Operation(
+        summary = "Registrar adoção",
+        description = "Registra uma nova adoção que ocorreu."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Registrado com sucesso!"),
+        @ApiResponse(responseCode = "400",
+            description = "O corpo da requisição está mal formulado, " +
+                "a data é inexistente/formato é inválido, " +
+                "algum dos campos está inválido, " +
+                "ou o pet informado já foi adotado!")
+    })
     public ResponseEntity<Void> inserirAdocao(@Valid @RequestBody AdocaoDTO dto) {
         service.inserir(dto);
         return ResponseEntity.status(201).build();
     }
 
     @PutMapping("/{id}")
+    @Operation(
+        summary = "Atualizar adoção",
+        description = "Atualiza uma adoção que já ocorreu."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Atualizado com sucesso!"),
+        @ApiResponse(responseCode = "400",
+            description = "O corpo da requisição está mal formulado, " +
+                "a data é inexistente/formato é inválido, " +
+                "algum dos campos está inválido, " +
+                "ou o pet informado já foi adotado!"),
+        @ApiResponse(responseCode = "404", description = "Adoção não encontrada!")
+    })
     public ResponseEntity<Void> atualizarAdocao(@Valid @RequestBody AdocaoDTO dto,
                                                 @PathVariable Integer id) {
         service.atualizar(dto,id);
@@ -44,6 +90,14 @@ public class AdocaoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+        summary = "Excluir adoção",
+        description = "Exclui uma adoção que já ocorreu pelo id."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Adoção excluída com sucesso!"),
+        @ApiResponse(responseCode = "404", description = "Adoção não encontrada!")
+    })
     public ResponseEntity<Void> excluirAdocaoPorId(@PathVariable Integer id) {
         service.excluirPorId(id);
         return ResponseEntity.status(200).build();
